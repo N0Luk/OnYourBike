685fb2e94ae1b9c04e1f8b044902cea2fa19c7fd
lukas noetzel <lukas.noetzel@gmail.com>
2023-03-14 17:51:17 +0100

    fixed seed and sign up

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 require 'faker'
+Booking.destroy_all
 Bike.destroy_all
 User.destroy_all
 
