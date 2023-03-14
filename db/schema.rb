c59faa790d2dd04b004f534c28f9e2513edfe77e
lukas noetzel <lukas.noetzel@gmail.com>
2023-03-14 11:58:41 +0100

    created seed bikes and users

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_13_163938) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_14_105007) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bikes", force: :cascade do |t|
-    t.string "type"
+    t.string "bike_type"
     t.string "name"
     t.float "price"
     t.string "location"
