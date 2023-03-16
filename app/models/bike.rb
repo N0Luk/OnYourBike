8c41555bd3e58267287d3da9af3bd7a503e19f5a
lukas noetzel <lukas.noetzel@gmail.com>
2023-03-16 14:47:57 +0100

    added search

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -2,4 +2,11 @@ class Bike < ApplicationRecord
   belongs_to :user
   has_many :bookings
   has_one_attached :photo
+  include PgSearch::Model
+
+  pg_search_scope :global_search,
+  against: [ :bike_type, :price, :location],
+  using: {
+    tsearch: { prefix: true }
+  }
 end
